e3a22d0b3bded7cdc6f2cc7b78247e75990ff702
Mirasik1 <aituaraferist7b@gmail.com>
2023-10-07 13:53:31 +0600

    Recognition of Position

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'components/position.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final PositionHelper _positionHelper = PositionHelper();
+  LocationData? _locationData;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Location Example'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _locationData != null
+                    ? 'Location: ${_locationData!.latitude}, ${_locationData!.longitude}'
+                    : 'Press the button to get location',
+              ),
+              ElevatedButton(
+                onPressed: _getLocation,
+                child: Text('Get Location'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _getLocation() async {
+    _locationData = await _positionHelper.getLocation();
+    setState(() {});
   }
 }
